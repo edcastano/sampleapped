705c744902dbfe125f3a3d6a1e60331dc7774566
Edward Castano <ed.castano@gmail.com>
2011-09-01 20:47:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapped::Application.config.secret_token = 'cfdc222ffd452464045a5c141f64ca13acc6898ce3ffb421f23c12446b293ec84b5e936f8be814fa6684689c8cd91036888ce078d0abf5a1dcbc0ba17041dfd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sampleapped::Application.config.session_store :cookie_store, :key => '_sampleapped_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sampleapped::Application.config.session_store :active_record_store
